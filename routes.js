79b8c9e949d100a4addd671e30c0ebb783c33584
ARJIT BHANDARI <43830893+Arjit222830@users.noreply.github.com>
2020-03-23 08:50:36 +0530

    security changes

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ module.exports= (app)=>{
     app.get('/payment',(req,res)=>{
         let params = {};
         
-        params['MID'] ='UFDOXs30620495877526',
+        params['MID'] ='MID
         params['WEBSITE']='WEBSTAGING',
         params['CHANNEL_ID'] = 'WEB',
         params['INDUSTRY_TYPE_ID']= 'Retail',
@@ -13,8 +13,8 @@ module.exports= (app)=>{
         params['CUST_ID']= 'CUST0011',
         params['TXN_AMOUNT']= '1',
         params['CALLBACK_URL']= 'http://localhost:3000/callback',
-        params['EMAIL']= 'siddharth0sinha@gmail.com',
-        params['MOBILE_NO']= '8076433986',
+        params['EMAIL']= 'dsds,
+        params['MOBILE_NO']= '000',
 
         checksum_lib.genchecksum(params,'ZIHZ_lhDJ3z3SeyC',function(err,checksum){
             let txn_url   = "https://securegw-stage.paytm.in/order/process";
